6fa172000c7a2147d2f90b2d5a1de1c46b103333
Harshavardhan K R <112190305+Harshavardhaann@users.noreply.github.com>
2025-01-13 15:53:30 +0530

    Add files via upload

---
+++ Lab Assignment 2/Untitled-1.js
@@ -0,0 +1,85 @@
+// Declare variables for the full name and age
+var fullName;
+var age;
+
+// Declare four variables without assigning values
+var var1, var2, var3, var4;
+
+// Declare four variables with assigned values
+var1=10, var2=11,var3=12,var3=14,var4=15;
+
+// Declare variables to store first name, last name, marital status, country, and age in multiple lines
+let First_Name= "Harsha";
+let Last_Name = "K";
+let Marital_Status = "Single";
+let Country = "INDIA";
+let Age =23;
+
+
+// Declare variables to store first name, last name, marital status, country, and age in a single line
+let First_Name2= "Harsha", Last_Name2 = "K",Marital_Status2 = "Single",Country2 = "INDIA",Age2 =23;
+
+// Declare two variables _myAge_ and _yourAge_ and assign them initial values
+  let myAge = 25;
+  let yourAge = 30;
+  
+// Log to the browser console
+console.log(`I am ${myAge} years old.`);
+console.log(`You are ${50} years old`);
+
+
+// Constant 
+// Declare a variable using var, let, and const, and log their initial values to the console.
+var myvar = "Using the VAR";
+let mylet = "Using the LET";
+const myconst ="Using the Const";
+
+console.log("Initial values:");
+console.log(myvar);
+console.log(mylet);
+console.log(myconst);
+
+// Try reassigning values to each variable.
+ myvar = "Reassigned var values";
+ mylet = "Reassigned let values"
+
+
+console.log(myvar);
+console.log(mylet);
+
+// What happens with const?
+/* The reason const variables cannot be reassigned in JavaScript is due to the way they are designed to behave. 
+When you declare a variable using const, 
+you're telling JavaScript that the variable's value is constant and should remain the same throughout the scope where it was declared */
+
+
+// Try redeclaring each variable in the same scope.
+
+var myvar = "Reassigned var again";
+// let mylet ="Reassigned let again";
+
+console.log(myvar);
+console.log(mylet);
+// Observe the behavior of var, let, and const.
+
+//Data Types
+//Declare variables and assign string, boolean, undefined and null data types
+//Whether you are a student (true/false).
+//Log the values and their types to the console using typeof.
+let name = "Harsha";
+let student = true;
+let undefinedVar;
+let log=null;
+
+console.log(name,typeof name);
+console.log(student,typeof student);
+console.log(undefinedVar, typeof undefinedVar);
+console.log(log, typeof log);
+
+// comments can make code readable
+/*
+  comments can make code readable,
+  easy to use and informative
+*/
+
+
